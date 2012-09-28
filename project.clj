e549ea1993df979812fdd57d3c8cc3b8524e80f8
Kyle S. Goodwin <goodwink@gmail.com>
2012-09-28 05:47:09 -0400

    Add midje testing plugin.

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
                  [compojure "1.1.3"]
                  [clojurewerkz/neocons "1.0.2"]
                  [midje "1.4.0"]]
-  :plugins [[lein-ring "0.7.1"]]
+  :plugins [[lein-ring "0.7.1"]
+            [lein-midje "2.0.0-SNAPSHOT"]]
   :ring {:handler treehousetag.core/api})
