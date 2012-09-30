e02c583e22e970a981045688140f93d3aa5a5e90
Kyle S. Goodwin <goodwink@gmail.com>
2012-09-30 10:08:48 -0400

    Refactor api to common patterns.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject treehousetag "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Treehouse Tag API Endpoints"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "4.0.3"]
                  [compojure "1.1.3"]
-                 [clojurewerkz/neocons "1.1.0-SNAPSHOT"]
+                 [clojurewerkz/neocons "1.1.0-beta1"]
                  [midje "1.4.0"]]
   :plugins [[lein-ring "0.7.1"]
             [lein-midje "2.0.0-SNAPSHOT"]]
--- src/treehousetag/core.clj
+++ src/treehousetag/core.clj
@@ -13,109 +13,105 @@
 (nr/create-index "node-type")
 (nsp/add-simple-point-layer "location")
 
-(defn relationship-other-id [node relationship]
+;;
+;; Implementation
+;;
+
+(defn- relationship-other-id [node relationship]
   (if (= (:start relationship) (:location-uri node))
     (last (split (:end relationship) #"/"))
     (last (split (:start relationship) #"/"))))
 
-(defn user-json [node]
+(defn- user-json [node]
   (let [friends (map (partial relationship-other-id node) (nr/outgoing-for node :types [:friend]))
         friends-of (map (partial relationship-other-id node) (nr/incoming-for node :types [:friend]))
         children (map (partial relationship-other-id node) (nr/outgoing-for node :types [:child]))]
     (generate-string (merge {:id (:id node) :friends friends :friends-of friends-of :children children} (:data node)))))
 
-(defn child-json [node]
+(defn- child-json [node]
   (let [friends (map (partial relationship-other-id node) (nr/outgoing-for node :types [:friend]))
         friends-of (map (partial relationship-other-id node) (nr/incoming-for node :types [:friend]))
         parents (map (partial relationship-other-id node) (nr/incoming-for node :types [:child]))
         interests (map (partial relationship-other-id node) (nr/outgoing-for node :types [:interest]))]
     (generate-string (merge {:id (:id node) :friends friends :friends-of friends-of :parents parents :interests interests} (:data node)))))
 
-(defn place-json [node]
+(defn- place-json [node]
   (let [businesses (map (partial relationship-other-id node) (nr/outgoing-for node :types [:business]))
         interests (map (partial relationship-other-id node) (nr/outgoing-for node :types [:interest]))]
     (generate-string (merge {:id (:id node) :businesses businesses :interests interests} (:data node)))))
 
-(defn business-json [node]
+(defn- business-json [node]
   (let [deals (map (partial relationship-other-id node) (nr/outgoing-for node :types [:deal]))
         interests (map (partial relationship-other-id node) (nr/outgoing-for node :types [:interest]))]
     (generate-string (merge {:id (:id node) :deals deals :interests interests} (:data node)))))
 
-(defn standard-json [node]
+(defn- standard-json [node]
   (generate-string (merge {:id (:id node)} (:data node))))
+
+(defn- create-from-body
+  ([req type json-func spatial]
+    (let [body (parse-string (slurp (:body req)))
+          item (nn/create (assoc body :type type))]
+        (nn/add-to-index item "node-type" "node-type" (name type))
+        (if spatial (nsp/add-node-to-layer "location" item))
+        (json-func item)))
+  ([id req type json-func spatial]
+    (let [body (parse-string (slurp (:body req)))
+          item (nn/create (assoc body :type type))]
+      (do
+        (nn/add-to-index item "node-type" "node-type" (name type))
+        (if spatial (nsp/add-node-to-layer "location" item))
+        (nr/create {:id id} {:id (:id item)} type)
+        (json-func (nn/get (Long/parseLong id)))))))
+
+(defn- attach-by-ids [type start end json-func]
+  (do
+    (nr/create {:id start} {:id end} type)
+    (json-func (nn/get (Long/parseLong start)))))
+
+;;
+;; API
+;;
 
 (defroutes api
   (GET "/users/:id" [id]
     (user-json (nn/get (Long/parseLong id))))
 
   (POST "/users" [:as req]
-    (let [body (parse-string (slurp (:body req)))
-          user (nn/create (assoc body :type :user))]
-      (nr/add-to-index user "node-type" "node-type" "user")
-      (nsp/add-node-to-layer "location" user)
-      (user-json user)))
+    (create-from-body req :user user-json true)
 
   (PUT "/users/:id/friends/:friend-id" [id friend-id]
-    (do
-      (nr/create {:id id} {:id friend-id} :friend)
-      (user-json (nn/get (Long/parseLong id)))))
+    (attach-by-ids :friend id friend-id user-json))
 
   (POST "/users/:id/children" [id :as req]
-    (let [body (parse-string (slurp (:body req)))
-          child-id (:id (nn/create (assoc body :type :child)))]
-      (do
-        (nr/create {:id id} {:id child-id} :child)
-        (child-json (nn/get (Long/parseLong id))))))
+    (create-from-body id req :child child-json false))
 
   (GET "/children/:id" [id]
     (child-json (nn/get (Long/parseLong id))))
 
   (PUT "/children/:id/friends/:friend-id" [id friend-id]
-    (do
-      (nr/create {:id id} {:id friend-id} :friend)
-      (child-json (nn/get (Long/parseLong id)))))
+    (attach-by-ids :friend id friend-id child-json))
 
   (PUT "/children/:id/interests/:interest-id" [id interest-id]
-    (do
-      (nr/create {:id id} {:id interest-id} :interest)
-      (child-json (nn/get (Long/parseLong id)))))
+    (attach-by-ids :interest id interest-id child-json))
 
   (POST "/interests" [:as req]
-    (let [body (parse-string (slurp (:body req)))
-          interest (nn/create (assoc body :type :interest))]
-      (nr/add-to-index interest "node-type" "node-type" "interest")
-      (interest-json interest)))
+    (create-from-body req :interest interest-json false))
 
   (GET "/interests" []
     (map interest-json (nn/find "node-type" "interest")))
 
-  (POST "/places" []
-    (let [body (parse-string (slurp (:body req)))
-          place (nn/create (assoc body :type :place))]
-      (nr/add-to-index place "node-type" "node-type" "place")
-      (nsp/add-node-to-layer "location" place)
-      (place-json place)))
+  (POST "/places" [:as req]
+    (create-from-body req :place place-json true))
 
   (PUT "/places/:id/interests/:interest-id" [id interest-id]
-    (do
-      (nr/create {:id id} {:id interest-id} :interest)
-      (place-json (nn/get (Long/parseLong id)))))
+    (attach-by-ids :interest id interest-id place-json))
 
   (PUT "/places/:id/businesses/:business-id" [id business-id]
-    (do
-      (nr/create {:id id} {:id business-id} :business)
-      (place-json (nn/get (Long/parseLong id)))))
+    (attach-by-ids :business id business-id place-json))
 
-  (POST "/businesses" []
-    (let [body (parse-string (slurp (:body req)))
-          business (nn/create (assoc body :type :business))]
-      (nr/add-to-index business "node-type" "node-type" "business")
-      (nsp/add-node-to-layer "location" business)
-      (business-json business)))
+  (POST "/businesses" [:as req]
+    (create-from-body req :business business-json false))
 
   (PUT "/businesses/:id/deals" [id :as req]
-    (let [body (parse-string (slurp (:body req)))
-          deal-id (:id (nn/create (assoc body :type :deal)))]
-      (do
-        (nr/create {:id id} {:id deal-id} :deal)
-        (standard-json (nn/get (Long/parseLong id)))))))
+    (create-from-body id req :deal standard-json false)))
